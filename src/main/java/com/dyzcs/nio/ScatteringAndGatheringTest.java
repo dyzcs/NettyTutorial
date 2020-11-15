25241509bb3420785535a66576cdf639c6459534
dyzcs <deng.kinger@gmail.com>
2020-11-15 12:23:01 +0800

    Create ScatteringAndGatheringTest.java

---
+++ src/main/java/com/dyzcs/nio/ScatteringAndGatheringTest.java
@@ -0,0 +1,12 @@
+package com.dyzcs.nio;
+
+/**
+ * Created by Administrator on 2020/11/15.
+ * <p>
+ * Scattering: 将数据写入到buffer时，可以采用buffer数组，依次写(分散)
+ * Gathering: 将数据读出到buffer时，可以采用buffer数组，依次读
+ */
+public class ScatteringAndGatheringTest {
+    public static void main(String[] args) {
+    }
+}
