006aa0479137fd5dc9e6e388f196b8f974b2365b
dyzcs <deng.kinger@gmail.com>
2020-11-15 15:14:27 +0800

    Update ScatteringAndGatheringTest.java

--- src/main/java/com/dyzcs/nio/ScatteringAndGatheringTest.java
+++ src/main/java/com/dyzcs/nio/ScatteringAndGatheringTest.java
@@ -1,5 +1,12 @@
 package com.dyzcs.nio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
 /**
  * Created by Administrator on 2020/11/15.
  * <p>
@@ -8,5 +15,35 @@ package com.dyzcs.nio;
  */
 public class ScatteringAndGatheringTest {
     public static void main(String[] args) {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(7000);
+
+            // 绑定端口到socket并启动
+            serverSocketChannel.socket().bind(inetSocketAddress);
+
+            // 创建buffer数组
+            ByteBuffer[] byteBuffers = new ByteBuffer[2];
+            byteBuffers[0] = ByteBuffer.allocate(5);
+            byteBuffers[1] = ByteBuffer.allocate(3);
+
+            // 等待客户端连接
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            // 假定从客户端接收8个字节
+            int messageLength = 8;
+            // 循环读取
+            while (true) {
+                int byteRead = 0;
+                while (byteRead < messageLength) {
+                    long l = socketChannel.read(byteBuffers);
+                    byteRead += l;
+                    System.out.println("byteRead=" + byteRead);
+                    // 使用流答应，当前buffer的position和limit
+                    Arrays.stream(byteBuffers).map(buffer -> "position=" + buffer.position() + ", limit=" + buffer.limit()).forEach(System.out::println);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
