73710643e9649ab99f6aa121032c90186ab85556
dyzcs <deng.kinger@gmail.com>
2020-11-15 21:09:30 +0800

    Create GroupChatServer.java

---
+++ src/main/java/com/dyzcs/nio/groupchat/GroupChatServer.java
@@ -0,0 +1,128 @@
+package com.dyzcs.nio.groupchat;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+/**
+ * Created by Administrator on 2020/11/15.
+ */
+public class GroupChatServer {
+    // 定义属性
+    private Selector selector;
+    private ServerSocketChannel listenChannel;
+    private static final int PORT = 6667;
+
+    // 构造器
+    // 初始化
+    public GroupChatServer() {
+        try {
+            // 得到选择器
+            selector = Selector.open();
+            // serverSocketChannel
+            listenChannel = ServerSocketChannel.open();
+            // 绑定端口
+            listenChannel.socket().bind(new InetSocketAddress(InetAddress.getLocalHost(), PORT));
+            // 设置非阻塞模式
+            listenChannel.configureBlocking(false);
+            // 将该listenChannel注册到selector
+            listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 监听
+    public void listen() {
+        try {
+            while (true) {
+                int count = selector.select(2000);
+                // 有事件处理
+                if (count > 0) {
+                    // 遍历得到的SelectionKey集合
+                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                    while (iterator.hasNext()) {
+                        // 取出selectionKey
+                        SelectionKey key = iterator.next();
+                        // 监听到accept
+                        if (key.isAcceptable()) {
+                            SocketChannel sc = listenChannel.accept();
+                            // 将该sc注册到selector
+                            sc.register(selector, SelectionKey.OP_READ);
+                            // 提示上线
+                            System.out.println(sc.getRemoteAddress() + " 上线");
+                        }
+                        // 通道发生read事件，通道是可读状态
+                        if (key.isReadable()) {
+                            // 处理读
+                            readData(key);
+                        }
+                        // 手动删除
+                        iterator.remove();
+                    }
+                } else {
+                    System.out.println("等待中");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    // 读取客户端消息
+    private void readData(SelectionKey key) {
+        // 定义一个socketChannel
+        SocketChannel channel = null;
+        try {
+            channel = (SocketChannel) key.channel();
+            // 创建buffer
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int count = channel.read(buffer);
+            // 根据count值做处理
+            if (count > 0) {
+                String msg = new String(buffer.array(), 0, count);
+                System.out.println("客户端消息" + msg);
+                // 向其他客户端转发消息
+                sendInfoToOtherClients(msg, channel);
+            }
+        } catch (IOException e) {
+            try {
+                System.out.println(channel.getRemoteAddress() + " 离线了");
+                // 取消注册
+                key.cancel();
+                // 关闭通道
+                channel.close();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    // 转发消息给其他客户
+    private void sendInfoToOtherClients(String msg, SocketChannel self) throws IOException {
+        System.out.println("服务器转发消息中");
+        // 遍历所有注册到selector上的SocketChannel，并排除self
+        for (SelectionKey key : selector.keys()) {
+            // 通过key取出对应的channel
+            Channel targetChannel = key.channel();
+            // 排除自己
+            if (targetChannel instanceof SocketChannel && targetChannel != self) {
+                // 转型
+                SocketChannel dest = (SocketChannel) targetChannel;
+                // 将msg存储到buffer
+                ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
+                // 将buffer的数据写入通道
+                dest.write(buffer);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
