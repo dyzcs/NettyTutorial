0a2e309553d5298f6618051b295d82589de799af
dyzcs <deng.kinger@gmail.com>
2020-11-15 09:31:26 +0800

    filechannel

---
+++ src/main/java/com/dyzcs/nio/NIOFileChannel01.java
@@ -0,0 +1,43 @@
+package com.dyzcs.nio;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Objects;
+
+/**
+ * Created by Administrator on 2020/11/15.
+ */
+public class NIOFileChannel01 {
+    public static void main(String[] args) {
+        String str = "hello, 中国";
+        FileChannel fileChannel = null;
+        try {
+            FileOutputStream fos = new FileOutputStream("output/file01.txt");
+            // 通过输出流获取对应的文件channel
+            // fileChannel真是类型是FileChannelImpl
+            fileChannel = fos.getChannel();
+
+            // 创建一个缓冲区byteBuffer
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+            // 将str放入byteBuffer
+            byteBuffer.put(str.getBytes());
+
+            // 对byteBuffer翻转
+            byteBuffer.flip();
+
+            // 将byteBuffer数据写入到fileChannel
+            fileChannel.write(byteBuffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                Objects.requireNonNull(fileChannel).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
