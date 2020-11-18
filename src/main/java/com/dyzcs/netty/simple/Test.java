100364c0d459b30ce3e015e51665c003beaa7222
dyzcs <deng.kinger@gmail.com>
2020-11-18 20:39:25 +0800

    Create Test.java

---
+++ src/main/java/com/dyzcs/netty/simple/Test.java
@@ -0,0 +1,12 @@
+package com.dyzcs.netty.simple;
+
+import io.netty.util.NettyRuntime;
+
+/**
+ * Created by Administrator on 2020/11/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(NettyRuntime.availableProcessors());
+    }
+}
