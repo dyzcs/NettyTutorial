0c7e98034be46185ba4a4674094c7d404ac71fb3
dyzcs <deng.kinger@gmail.com>
2020-11-20 12:14:06 +0800

    Update MyServer.java

--- src/main/java/com/dyzcs/netty/heartbeat/MyServer.java
+++ src/main/java/com/dyzcs/netty/heartbeat/MyServer.java
@@ -1,18 +1,34 @@
 package com.dyzcs.netty.heartbeat;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 /**
  * Created by Administrator on 2020/11/20.
  */
 public class MyServer {
     public static void main(String[] args) throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
-            
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+
+                }
+                });
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
