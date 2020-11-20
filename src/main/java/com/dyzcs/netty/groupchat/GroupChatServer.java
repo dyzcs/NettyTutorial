d1f9fc44eaf51a99b978cf3db5c8a1a33adf8eb7
dyzcs <deng.kinger@gmail.com>
2020-11-20 09:48:23 +0800

    netty groupChat server

--- src/main/java/com/dyzcs/netty/groupchat/GroupChatServer.java
+++ src/main/java/com/dyzcs/netty/groupchat/GroupChatServer.java
@@ -1,27 +1,60 @@
 package com.dyzcs.netty.groupchat;
 
-import io.netty.channel.EventLoopGroup;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * Created by Administrator on 2020/11/19.
  */
 public class GroupChatServer {
     // 监听端口
-    private int port;
+    private final int port;
 
     public GroupChatServer(int port) {
         this.port = port;
     }
 
     // 处理客户端请求
-    public void run() {
+    public void run() throws Exception {
         // 创建两个线程组
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            // 获取到pipeline
+                            ChannelPipeline pipeline = ch.pipeline();
+                            // 向pipeline中加入解码器
+                            pipeline.addLast("decoder", new StringDecoder());
+                            // 向pipeline中加入编码器
+                            pipeline.addLast("encoder", new StringEncoder());
+                            // 加入自己的业务处理
+                            pipeline.addLast(new GroupChatServerHandler());
+                        }
+                    });
+
+            System.out.println("netty服务器启动");
+            ChannelFuture cf = bootstrap.bind(port).sync();
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        new GroupChatServer(9999).run();
     }
 }
---
+++ src/main/java/com/dyzcs/netty/groupchat/GroupChatServerHandler.java
@@ -0,0 +1,67 @@
+package com.dyzcs.netty.groupchat;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2020/11/20.
+ */
+public class GroupChatServerHandler extends SimpleChannelInboundHandler<String> {
+    // 定义一个channel组，管理所有的channel
+    // GlobalEventExecutor.INSTANCE是全局的事件执行器，是一个单例
+    private static final ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    // handlerAdded: 表示建立连接，一旦连接，第一个执行
+    // 将当前channel加入到channelGroup
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) {
+        Channel channel = ctx.channel();
+        // 将该客户加入聊天的信息推送给其他所有在线的客户端
+        channelGroup.writeAndFlush("[客户端]" + channel.remoteAddress() + " 加入聊天" + sdf.format(new Date()) + "\n");
+        channelGroup.add(channel);
+    }
+
+    // 断开连接，将该客户端下线的消息推送给当前在线的客户
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush("[客户端]" + channel.remoteAddress() + " 离开了\n");
+        System.out.println("channelGroup size = " + channelGroup.size());
+    }
+
+    // 表示channel处于活动状态，提示xx上线
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println(ctx.channel().remoteAddress() + " 上线了\n");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println(ctx.channel().remoteAddress() + " 离线了\n");
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+        Channel channel = ctx.channel();
+        channelGroup.forEach(ch -> {
+            if (channel != ch) {
+                ch.writeAndFlush("[客户]" + channel.remoteAddress() + " 发送了消息" + msg + "\n");
+            } else {
+                ch.writeAndFlush("[自己]发送了消息" + msg + "\n");
+            }
+        });
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
