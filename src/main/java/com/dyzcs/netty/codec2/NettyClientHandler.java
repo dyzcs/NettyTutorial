062db30ad1b49e75a87624322ff48c1546cf9b04
dyzcs <deng.kinger@gmail.com>
2020-11-21 13:39:52 +0800

    proto codec2

---
+++ src/main/java/com/dyzcs/netty/codec2/NettyClientHandler.java
@@ -0,0 +1,34 @@
+package com.dyzcs.netty.codec2;
+
+import com.dyzcs.netty.codec.StudentPOJO;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by Administrator on 2020/11/18.
+ */
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    // 当通道就绪就会触发该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        // 发送一个Student对象到服务器
+        StudentPOJO.Student student = StudentPOJO.Student.newBuilder().setId(4).setName("宋江").build();
+        ctx.writeAndFlush(student);
+    }
+
+    // 当通道有读取事件时，会触发
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器的地址" + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/dyzcs/netty/codec2/NettyServerHandler.java
@@ -0,0 +1,36 @@
+package com.dyzcs.netty.codec2;
+
+import com.dyzcs.netty.codec.StudentPOJO;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by Administrator on 2020/11/17.
+ * <p>
+ * 1.自定义一个handler需要继承netty规定好的某个HandlerAdapter
+ * 2.这时自定义的Handler，才能成为一个handler
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // 读取从客户端发送的StudentPOJO.Student
+        StudentPOJO.Student student = (StudentPOJO.Student) msg;
+        System.out.println("客户端发送的数据 id = " + student.getId() + ", name = " + student.getName());
+    }
+
+    // 数据读取完毕
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        // writeAndFlush: 将数据写入到缓冲并刷新
+        // 一般来讲，要对发送的数据进行编码
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello client", CharsetUtil.UTF_8));
+    }
+
+    // 处理异常，一般是要关闭通道
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
