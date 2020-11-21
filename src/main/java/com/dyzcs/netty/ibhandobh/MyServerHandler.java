65cc2dc97b0759ac85193e05c31c50425aecb8f0
dyzcs <deng.kinger@gmail.com>
2020-11-21 20:21:35 +0800

    Update MyServerHandler.java

--- src/main/java/com/dyzcs/netty/ibhandobh/MyServerHandler.java
+++ src/main/java/com/dyzcs/netty/ibhandobh/MyServerHandler.java
@@ -8,12 +8,12 @@ import io.netty.channel.SimpleChannelInboundHandler;
  */
 public class MyServerHandler extends SimpleChannelInboundHandler<Long> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Long msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, Long msg) {
         System.out.println("从客户端" + ctx.channel().remoteAddress() + " 读取到long " + msg);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
     }
