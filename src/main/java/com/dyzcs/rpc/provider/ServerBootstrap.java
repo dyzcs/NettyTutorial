250ba177e545c0e4da5676305c0fe78ea3c57234
dyzcs <deng.kinger@gmail.com>
2020-11-22 09:48:03 +0800

    Create ServerBootstrap.java

---
+++ src/main/java/com/dyzcs/rpc/provider/ServerBootstrap.java
@@ -0,0 +1,12 @@
+package com.dyzcs.rpc.provider;
+
+/**
+ * Created by Administrator on 2020/11/22.
+ * <p>
+ * ServerBootstrap: 启动一个服务提供者
+ */
+public class ServerBootstrap {
+    public static void main(String[] args) {
+
+    }
+}
